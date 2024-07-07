5a2d47a9c77b15508badda2c2256189e0b3f2719
Niccolò <123510358+NicusPulcis@users.noreply.github.com>
2024-07-07 19:52:47 +0200

    Update ability-trigger.ts [ Localization(it) ]

--- src/locales/it/ability-trigger.ts
+++ src/locales/it/ability-trigger.ts
@@ -3,10 +3,10 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "{{abilityName}} di {{pokemonName}}\nl'ha protetto dal contraccolpo!",
   "badDreams": "{{pokemonName}} è tormentato dagli incubi!",
-  "costar": "{{pokemonName}} copied {{allyName}}'s stat changes!",
+  "costar": "{{pokemonName}} ha copiato le modifiche alle statistiche di {{allyName}}!",
   "iceFaceAvoidedDamage": "{{pokemonName}} ha evitato\ni danni grazie a {{abilityName}}!",
-  "trace": "{{pokemonName}} copied {{targetName}}'s\n{{abilityName}}!",
+  "trace": "{{pokemonName}} ha copiato {{abilityName}} di\n{{targetName}}!",
   "windPowerCharged": "Venire colpito da {{moveName}} ha caricato {{pokemonName}}!",
-  "quickDraw":"{{pokemonName}} can act faster than normal, thanks to its Quick Draw!",
-  "disguiseAvoidedDamage" : "{{pokemonName}}'s disguise was busted!",
+  "quickDraw":"{{pokemonName}} può agire per primo, grazie a Colpolesto!",
+  "disguiseAvoidedDamage" : "{{pokemonName}} è stato smascherato!",
 } as const;
